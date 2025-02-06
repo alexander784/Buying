c59f43f4c5392b669a24871009b5d02a25a05a16
alexander784 <alexandernyaga1999@gmail.com>
2025-02-06 14:54:40 +0300

    Configure db

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -74,13 +80,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD':'@12345',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Password validation
