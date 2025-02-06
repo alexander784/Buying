d11fab2cc7b70ff22028b938f0c73508b93c8ade
alexander784 <alexandernyaga1999@gmail.com>
2025-02-06 15:26:48 +0300

    Register user

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from django.contrib.auth import get_user_model,authenticate
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+User = get_user_model()
+
+
+@api_view('POST')
+@permission_classes([AllowAny])
+def register_user(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
