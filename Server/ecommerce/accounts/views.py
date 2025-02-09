74ac9e069b9df5aaedd27355e5af9f808345478a
alexander784 <alexandernyaga1999@gmail.com>
2025-02-09 22:14:41 +0300

    Create user account

---
+++ Server/ecommerce/accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from django.contrib.auth import get_user_model,authenticate
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import RegisterSerializer,LoginSerializer,UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from django.http import JsonResponse
+
+
+# Create your views here.
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    serializer = LoginSerializer(data=request.data)
+    if not serializer.is_valid():
+        return JsonResponse({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    user = serializer.validated_data['user']
+
+    refresh = RefreshToken.for_user(user)
+    return Response({
+        'refresh':str(refresh),
+        'access':str(refresh.access_token),
+        'user':UserSerializer(user).data
+    }, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_details(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+
