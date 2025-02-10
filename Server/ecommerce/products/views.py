0bbbaff758518f906c37f591b489d071468f91a7
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 06:46:47 +0300

    Get all products

--- Server/ecommerce/products/views.py
+++ Server/ecommerce/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .serializer import ProductSerializer
+from .models import Product
+
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTP_200_OK)
