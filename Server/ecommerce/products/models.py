522dec92b7e74f17ee43f78c6e6365687c731366
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 12:33:28 +0300

    Add categories url& connect with products

--- Server/ecommerce/products/models.py
+++ Server/ecommerce/products/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    name =  models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='products/', blank=True,null=True)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='products')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-    
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,related_name='subcategories')
@@ -22,6 +9,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Product(models.Model):
+    name =  models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', blank=True,null=True)
+    category = models.ForeignKey('products.Category', on_delete=models.CASCADE,
+                                 related_name='products',
+                                 null=True,
+                                   blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
     
 
     
--- Server/ecommerce/products/urls.py
+++ Server/ecommerce/products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import get_all_products,create_product,get_product,update_product,delete_product
+from .views import get_all_products,create_product,get_product,update_product,delete_product,create_category,get_categories,category_detail
+
 
 
 urlpatterns = [
@@ -8,6 +9,9 @@ urlpatterns = [
     path('products/<int:product_id>/', get_product, name='get_product'),
     path('products/update/<int:product_id>/', update_product, name='update_product'),
     path('products/delete/<int:product_id>/', delete_product, name='delete_product'),
+    path('categories/',get_categories, name='get_categories'),
+    path('categories/create/', create_category, name='created_category'),
+    path('categories/<int:category_id>/', category_detail, name='category_detail'),
 
 
          
