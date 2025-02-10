8ce5e6fd7cb22933f5e1c5a39ec1588503a73a0b
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 11:48:51 +0300

    Add products category

--- Server/ecommerce/products/models.py
+++ Server/ecommerce/products/models.py
@@ -7,11 +7,22 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/', blank=True,null=True)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,related_name='subcategories')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
         return self.name
+    
+
     
     
