80ecd279a0f5283a605c50be9ba9edf6a8650ad2
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 11:55:59 +0300

    POst a new product category

--- Server/ecommerce/products/views.py
+++ Server/ecommerce/products/views.py
@@ -67,3 +67,13 @@ def get_categories(request):
     serializer = CategorySerializer(categories, many=True)
     return Response(serializer.data)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def create_category(request):
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
