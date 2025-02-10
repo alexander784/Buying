3be21bde70e0a05670c7ed18ffee197bd62aa027
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 11:39:47 +0300

    Manage products urls

--- Server/ecommerce/products/urls.py
+++ Server/ecommerce/products/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import get_all_products,create_product
+from .views import get_all_products,create_product,get_product,update_product,delete_product
 
 
 urlpatterns = [
     path('products/', get_all_products, name='get_all_products'),
     path('products/create/', create_product, name='create_product'),
+    path('products/<int:product_id>/', get_product, name='get_product'),
+    path('products/update/<int:product_id>/', update_product, name='update_product'),
+    path('products/delete/<int:product_id>/', delete_product, name='delete_product'),
+
 
          
 ]
