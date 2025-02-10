39661e6680af3901fd00ef32086ab9d062e2f9e5
alexander784 <alexandernyaga1999@gmail.com>
2025-02-10 07:09:34 +0300

    Create abd get all products

--- Server/ecommerce/products/views.py
+++ Server/ecommerce/products/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .serializer import ProductSerializer
 from .models import Product
+from django.shortcuts import get_object_or_404
+
 
 
 # Create your views here.
@@ -13,4 +15,23 @@ from .models import Product
 def get_all_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data, status=status.HTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
