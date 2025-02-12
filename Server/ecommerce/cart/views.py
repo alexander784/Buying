55d95f22797341e6549a74050f3a1623ae2c33c0
alexander784 <alexandernyaga1999@gmail.com>
2025-02-12 12:57:20 +0300

    Add to cart

--- Server/ecommerce/cart/views.py
+++ Server/ecommerce/cart/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Product
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        cart[str(product_id)]['quantity'] += 1
+    else:
+        cart[str(product_id)] = {
+            'name':product.name,
+            'price':product.price,
+            'quantity': 1
+        }
+    request.session['cart'] = cart
+    return Response({"message":"Item added to cart", "cart": cart},status=status.HTTP_200_OK)
+
+        
+
+
