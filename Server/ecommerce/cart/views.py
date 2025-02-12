aeb401ed91fdb89e2b09a728445d905080adbc15
alexander784 <alexandernyaga1999@gmail.com>
2025-02-12 13:03:54 +0300

    Database cart for loggedin users

--- Server/ecommerce/cart/views.py
+++ Server/ecommerce/cart/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .models import Product
+from .models import Product,CartItem
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+
 
 
 # Create your views here.
+
+
+# Session based cart got guest
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -25,6 +31,24 @@ def add_to_cart(request, product_id):
         }
     request.session['cart'] = cart
     return Response({"message":"Item added to cart", "cart": cart},status=status.HTTP_200_OK)
+
+# Logged in user
+@receiver(user_logged_in)
+def transfer_cart_to_db(sender, request, user, **kwargs):
+    session_cart = request.session.get('cart', {})
+
+    for product_id, item in session_cart.items():
+        product = get_object_or_404(Product, id=product_id)
+        cart_item, created = CartItem.objects.get_or_create(
+            user=user, product=product,
+            defaults={"quantity":item['quantity']}
+        )
+        if not created:
+            cart_item.quantity += item["quantity"]
+            cart_item.save()
+
+    request.session['cart'] = {}
+
 
         
 
