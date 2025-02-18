092659d3e63d4bb6aa6bd117ebebcacb804795df
alexander784 <alexandernyaga1999@gmail.com>
2025-02-18 12:46:16 +0300

    Handle API request

--- client/pages/services/Api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/";
-
-
---
+++ client/services/Api.js
@@ -0,0 +1,22 @@
+import config from '@/postcss.config.mjs';
+import axios from 'axios';
+import { headers } from 'next/headers';
+
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/";
+
+export const Api = axios.create({
+    baseURL:API_BASE_URL,
+    headers: {
+        "Content-Type":"application/json",
+    },
+});
+    // Handle Tokens for JWT
+Api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+
+    }
+    return config;
+})
