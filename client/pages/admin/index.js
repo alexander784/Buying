87ffe139dd16c80e4b09c1b0c72761576ddc960f
alexander784 <alexandernyaga1999@gmail.com>
2025-02-20 07:27:43 +0300

    Admin dashboard

---
+++ client/pages/admin/index.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const AdminDashboard = () => {
+    return (  
+        <div className="max-w-4xlmx-auto p-6 bg-white shadow-lg rounded-md">
+            <h2 className="text-3xl font-bold mb-6">Admin Dashboard</h2>
+
+            <div className="space-y-4">
+            <Link href="/admin/AddProduct" className="block bg-orange-950 text-white p-3 rounded text-center">
+            Add New product
+            </Link>
+            <Link href="/admin/ProductsPage" className="block bg-orange-950 text-white p-3 rounded text-center">
+            View all Products
+            </Link>
+        </div>
+        </div>
+    );
+}
+ 
+export default AdminDashboard;
