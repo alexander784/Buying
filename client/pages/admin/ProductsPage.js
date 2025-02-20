e639ea6dbd84850ab00846d835d973b46c511706
alexander784 <alexandernyaga1999@gmail.com>
2025-02-20 08:10:11 +0300

    Fetch Admin products

--- client/pages/admin/ProductsPage.js
+++ client/pages/admin/ProductsPage.js
@@ -1,4 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const ProductsPage = () => {
+
+    const[products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            const token = localStorage.getItem('access_token');
+            const res =await axios.get(`${API_URL}/products/products/`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+
+                },
+            });
+            setProducts(res.data);
+            
+            }catch(error) {
+                console.error('Error fetchong products:', error)
+        }
+    }
+
+    
     return ( 
         <div className="">
             <h2>All Products</h2>
