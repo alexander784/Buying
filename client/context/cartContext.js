e7f0813b3899f9623e622a0386c02f4f1c3002b1
alexander784 <alexandernyaga1999@gmail.com>
2025-02-20 14:29:30 +0300

    Implement cart context

--- client/context/cartContext.js
+++ client/context/cartContext.js
@@ -1,6 +1,6 @@
-import { getCart } from "@/services/Cart";
+import { addToCart, getCart, requestQuote } from "@/services/Cart";
 
-const { createContext, useState, useEffect } = require("react");
+const { createContext, useState, useEffect, useContext } = require("react");
 
 
 const CartContext = createContext();
@@ -16,5 +16,31 @@ export const CartProvider = () => {
         const data = await getCart();
         setCart(data.cart);
     };
-    
-}
+
+    const handleAddToCart = async(productId) => {
+        await addToCart(productId);
+        fetchCart();
+    };
+
+    const handleRemoveFromCart = async(productId) => {
+        await removeFromCart(productId);
+        fetchCart();
+    };
+    const handleRequestQuote = async () => {
+        const data = await requestQuote();
+        if(data.whatsapp_link) {
+            window.open(data.whatsapp_link, "_blank");
+        }
+    };
+
+    return (
+        <CartContext.Provider
+        value={{ cart, handleAddToCart, handleRemoveFromCart, handleRequestQuote}}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+
+};
+
+export const useCart = () => useContext(CartContext);
