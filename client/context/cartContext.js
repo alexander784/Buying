cb94edc463a6d0671a1d4c5c284ce1b900271905
alexander784 <alexandernyaga1999@gmail.com>
2025-02-20 14:23:29 +0300

    Fetch cart

--- client/context/cartContext.js
+++ client/context/cartContext.js
@@ -1,5 +1,20 @@
-const { createContext } = require("react");
+import { getCart } from "@/services/Cart";
+
+const { createContext, useState, useEffect } = require("react");
 
 
 const CartContext = createContext();
 
+export const CartProvider = () => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetchCart();
+    }, []);
+
+    const fetchCart = async () => {
+        const data = await getCart();
+        setCart(data.cart);
+    };
+    
+}
