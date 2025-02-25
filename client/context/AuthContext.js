40cba059a048398090a9c88263d08e482156cf30
alexander784 <alexandernyaga1999@gmail.com>
2025-02-25 10:12:12 +0300

    Fetch user func

--- client/context/AuthContext.js
+++ client/context/AuthContext.js
@@ -1,5 +1,28 @@
-const { createContext } = require("react");
+const { createContext, useState, useEffect } = require("react");
 
 
 const AuthContext = createContext();
 
+export const AuthProvider = () => {
+    const [user, setUser] = useState(null);
+    const [role, setRole] = useState(null);
+
+
+
+    useEffect (() => {
+        const fetchUser = async () => {
+            const token = getToken();
+            if (token) {
+                const fetchedUser = await getUser();
+                if (fetchedUser) {
+                    setUser(fetchedUser);
+                    setRole(fetchedUser.is_admin ? 'admin' : 'user');
+                }
+            }
+        };
+        fetchUser();
+    }, []);
+
+
+    
+}
