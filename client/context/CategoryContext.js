6925268907844a41149bc9aa7263fd9952cca253
alexander784 <alexandernyaga1999@gmail.com>
2025-02-28 21:03:38 +0300

    Fetch categories

--- client/context/CategoryContext.js
+++ client/context/CategoryContext.js
@@ -1,3 +1,29 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const CategoryContext = createContext();
+
+export const CategoryProvider = () => {
+    const[categories, setCategories] = useState([]);
+    const[loadConfig, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+
+    const fetchCategories = async() => {
+        try {
+            const res = await fetch('');
+            if (!res.ok) throw new Error('Failed to fetch');
+            const data = await res.json();
+            setCategories(data);
+        } catch (err) {
+            setError(err.message);
+        }finally{
+            setLoading(false);
+        }
+    };
+    
+}
