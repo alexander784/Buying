2c50275eb3a73c9fb51abca81e7ccedce369ece7
alexander784 <alexandernyaga1999@gmail.com>
2026-03-05 13:36:43 +0300

    Use supabase db

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,6 @@ import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/cartContext";
 import { ProductsProvider } from "@/context/ProductsContext";
 import "@/styles/globals.css";
-import CategoriesPage from "./admin/categories";
 import { CategoryProvider } from "@/context/CategoryContext";
 
 export default function App({ Component, pageProps }) {
